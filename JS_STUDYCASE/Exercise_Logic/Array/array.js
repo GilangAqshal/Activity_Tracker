8ff738c32b3392f899e52c2c15e58e25b712edb2
GilangAqshal <gilangais16@gmail.com>
2025-03-19 21:52:50 +0700

    Array

---
+++ JS_STUDYCASE/Exercise_Logic/Array/array.js
@@ -0,0 +1,7 @@
+
+document.writeln(`<h1>Array Javascript</h1>`)
+        let names = ["Gilang", "Aqshal", "Ilham", "Safatulloh", "Putra"];
+
+        for(let i=0; i < names.length; i++){
+            document.writeln(`<p>${i+1}. ${names[i]}</p>`)
+}
