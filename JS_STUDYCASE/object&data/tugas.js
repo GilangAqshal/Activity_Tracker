9491fd58aa6713db41c1874afa263ba90de7cd4a
GilangAqshal <gilangais16@gmail.com>
2025-03-27 11:09:46 +0700

    object7DAta

---
+++ JS_STUDYCASE/object&data/tugas.js
@@ -0,0 +1,8 @@
+let Mahasiswa = {
+    nama: "Gilang Aqshal",
+    age: 18,
+    jurusan: "Informatika",
+    ipk: "3.9"
+}
+
+document.writeln(`Nama: ${Mahasiswa.nama} <br> Umur: ${Mahasiswa.age} <br> Jurusan: ${Mahasiswa.jurusan} <br> IPK: ${Mahasiswa.ipk}`)
