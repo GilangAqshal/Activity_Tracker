d97df17d63c63174bb5da87afdef64e95b745036
GilangAqshal <gilangais16@gmail.com>
2025-04-15 08:27:59 +0700

    update latJAVA

---
+++ JAVA_STUDYCASE/LatihanJava/LatihanVar.java
@@ -0,0 +1,29 @@
+package latihanjava;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gilan
+ */
+public class LatihanVar {
+    public static void main(String[] args){
+//        ini program
+//        System.out.println("Selamat Belajar Java Programming");
+        int Jarijari;
+        double keliling;
+        final double PI=3.14;
+        
+        Jarijari = 30;
+        
+        keliling = 2*PI*Jarijari;
+        System.out.println("Keliling Lingkaran = "+keliling);
+        System.out.println("====================="); 
+    }
+    
+}
+
