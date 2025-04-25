f013748ddfa6a982a86c330e62e941b67c795d65
GilangAqshal <gilangais16@gmail.com>
2025-04-25 21:34:11 +0700

    update

--- JS_STUDYCASE/object&data/tugas.js
+++ JS_STUDYCASE/object&data/tugas.js
@@ -2,7 +2,7 @@ let Mahasiswa = {
     nama: "Gilang Aqshal",
     age: 18,
     jurusan: "Informatika",
-    ipk: "3.9"
+    ipk: "3.7"
 }
 
 console.log(`Nama: ${Mahasiswa.nama} <br> Umur: ${Mahasiswa.age} <br> Jurusan: ${Mahasiswa.jurusan} <br> IPK: ${Mahasiswa.ipk}`)
