aadca5a9c3f43e266d0790e31a929526c9036b39
GilangAqshal <gilangais16@gmail.com>
2025-05-13 22:19:38 +0700

    update codewaes

--- Codewars/1-grow.js
+++ Codewars/1-grow.js
@@ -1,14 +1,14 @@
-// function grow(x){
-//     let result = x[0];
-//     for(let i = 1; i < x.length; i++){
-//         result = result * x[i];
-//     }
-
-//     return result;
-// }
-
 function grow(x){
-    return x.reduce((a, b) => a * b,1);
+    let result = x[0];
+    for(let i = 1; i < x.length; i++){
+        result = result * x[i];
+    }
+
+    return result;
 }
+
+// function grow(x){
+//     return x.reduce((a, b) => a * b,1);
+// }
 console.log(grow([2, 2, 2, 2, 2, 2]));
 
