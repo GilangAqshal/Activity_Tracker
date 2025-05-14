c96fe9eb3ddfa129a3b0a2af28ac7cf8c5734f24
GilangAqshal <gilangais16@gmail.com>
2025-05-14 11:23:31 +0700

    Day 1/366

--- Codewars/1-grow.js
+++ Codewars/1-grow.js
@@ -1,3 +1,8 @@
+//WPU Coding Challenge 
+// Day 1/366
+// Reduce But Grow
+// https://www.codewars.com/kata/56efab15740d301ab40002ee
+
 function grow(x){
     let result = x[0];
     for(let i = 1; i < x.length; i++){
