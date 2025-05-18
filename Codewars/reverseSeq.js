7f736abb90875a75fc28e1e446e5eb2a5fabb505
GilangAqshal <gilangais16@gmail.com>
2025-05-18 12:49:54 +0700

    Update Code in reverseSeq

--- Codewars/reverseSeq.js
+++ Codewars/reverseSeq.js
@@ -1,12 +1,13 @@
-const reverseSeq = n => {
+// const reverseSeq = n => {
 
- const result = [];
-  for(let i = n; i >= 1; i--){
-    result.push(i);
-  }
-    return result;
-};
+//  const result = [];
+//   for(let i = n; i >= 1; i--){
+//     result.push(i);
+//   }
+//     return result;
+// };
 
 // const reverseSeq = n => [...Array(n)].map((el, i) = > n - 1)
+const reverseSeq = n => [...Array(n)].map((el, i) => n - 1)
 
  console.log(reverseSeq(5));
